7dfefc8d347b1c13eb85c83bd0d58c7c889241c7
sig-tesla <keith.hawes@gmail.com>
2023-04-24 18:05:11 -0700

    tweak killing pianobar to use kill if "q" doesn't work -- piano bar hung

--- patiobar.sh
+++ patiobar.sh
@@ -9,7 +9,7 @@ case "$1" in
   startup)
 	# used on startup
 	echo  Pianobar startup...
-	sleep 5
+	sleep 10
 	hostname
 	hostname -I
 	$0 start-patiobar > /dev/null
@@ -25,7 +25,7 @@ case "$1" in
         pushd . > /dev/null
         cd $PIANOBAR_DIR
         pb_pid=$(pidof pianobar)
-        [[ "$pb_pid" -eq "" ]] && echo Starting pianobar && ($PIANOBAR_BIN > /dev/null 2>&1 &)
+        [[ "$pb_pid" -eq "" ]] && echo starting pianobar && ($PIANOBAR_BIN > /dev/null 2>&1 &)
         EXITSTATUS=$(($? + $EXITSTATUS))
 	# start in a stopped state
         echo -n 'P'>$PATIOBAR_DIR/ctl
@@ -66,7 +66,7 @@ case "$1" in
         pb_pid=$(pidof pianobar)
         [[ "$pb_pid" -ne "" ]] && echo killing $2 pianobar && kill $2 $pb_pid
        	EXITSTATUS=$?
-	pb_pid=$(ps -ef | grep "[n]ode index.js patiobar" | tr -s ' ' | cut -d ' ' -f2)
+      	pb_pid=$(ps -ef | grep "[n]ode index.js patiobar" | tr -s ' ' | cut -d ' ' -f2)
         [[ "$pb_pid" -ne "" ]] && echo stopping $2 patiobar && kill $2 $pb_pid
         EXITSTATUS=$(($? + $EXITSTATUS))
         exit $EXITSTATUS
@@ -83,19 +83,21 @@ case "$1" in
   stop-pianobar)
 	EXITSTATUS=0
         pb_pid=$(pidof pianobar)
+        # try the easy way by sending "q"uit to pianobar
         [[ "$pb_pid" -ne "" ]] && echo quitting pianobar && echo -n 'q' > $PATIOBAR_DIR/ctl
-        #[[ "$pb_pid" -ne "" ]] && sleep 5
-	#pb_pid2=$(pidof pianobar)
-	#[[ "$pb_pid2" -ne "" ]] && echo killing pianobar && kill $pb_pid2
+        [[ "$pb_pid" -ne "" ]] && sleep 5
+	      pb_pid2=$(pidof pianobar)
+	      # still there?  killit
+	      [[ "$pb_pid2" -ne "" ]] && echo killing pianobar && kill $pb_pid2
         EXITSTATUS=$?
         exit $EXITSTATUS
         ;;
   restart|force-reload)
         EXITSTATUS=0
-        $0 stop || EXITSTATUS=1
+        $0 stop
         sleep 5
-        $0 start || EXITSTATUS==$(($? + $EXITSTATUS))
-        exit $EXITSTATUS
+        $0 start
+        exit $?
         ;;
   status)
         # more of a list than a status, since this doesn't check values
@@ -127,7 +129,7 @@ case "$1" in
         exit 0   # if line above is uncommented, this will never be reached.
         ;;
   *)
-        echo "Usage: $0 {start |stop | start-patiobar | stop-pianobar |restart |status | status-pianobar | system-stop | system-reboot | testmode }" >&2
+        echo "Usage: $0 {start |stop | start-patiobar | stop-pianobar |restart |status | status-pianobar | system-stop | system-reboot | testmode | kill [-9]}" >&2
         exit 3
         ;;
 esac
